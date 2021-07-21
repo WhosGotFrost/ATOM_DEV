0cc568eb949eb07aa69326fbb3f6669ed1757a76
WhosGotFrost <>
2021-07-20 23:36:51 -0400

    main

---
+++ python/simpleauth.py
@@ -0,0 +1,8 @@
+#This simple ass auth can be use anywhere does not require a db...
+
+name = input('Hey, What is your name: ');
+
+if(name == 'frost'):
+    print("Welcome back " + name + ", Enjoy your time here!");
+else:
+    print('Who are you again?');
