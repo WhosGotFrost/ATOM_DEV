53bf52bb8c94acb25b42e3b4f9f6daa57b33b35d
WhosGotFrost <>
2021-07-21 13:43:42 -0400

    main

--- python/calulator.py
+++ python/calulator.py
@@ -1,12 +1,12 @@
 #A simple calulator
-
+#try catch will catch an error if number or number2 is not a number
 try:
     number = int(input('Enter first number: '));
     number2 = int(input('Enter second number: '));
 
-
     operator = input("Enter a operator: ");
 
+#checks if the users added a valid operator.
     if(operator == "+"):
         print("Your answer: ", number + number2);
     elif(operator == "-"):
